f0d6831bc0dc05340ff38560797ab61d20ab0ce3
Florian <ge95kek@mytum.de>
2022-11-18 18:18:43 +0100

    Initial commit

---
+++ backend/src/test/java/de/hkwh/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hkwh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
