cd63b0d66e2562d7d7ba74836aa780efdea2fb08
mariusweigt <marius.weigt@tum.de>
2022-11-19 16:51:02 +0100

    push 9

--- frontend/src/Dashboard/Info/DashboardInfoComponent.tsx
+++ frontend/src/Dashboard/Info/DashboardInfoComponent.tsx
@@ -1,7 +1,7 @@
 const DashboardInfoComponent = () => {
     return (
-        <div className="py-5 dashboard-info-component">
-            <text className="pt-4 text-white">
+        <div className="py-5 dashboard-info-component px-4">
+            <text className="pt-3 text-white">
                 Hey Ben,<br/> you are the today's vehicle manager.
             </text>
         </div>
