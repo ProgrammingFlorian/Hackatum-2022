48b94f139feb3f8d7cbe86035643d5d6699a1ac2
Florian <ge95kek@mytum.de>
2022-11-19 01:51:56 +0100

    started frontend

---
+++ frontend/src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM, {Root} from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+// CSS Files
+// add bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./animations.css";
+
+const root: Root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+// add 'bg-black' class to root element for dark background
+document.body.classList.add('bg-black');
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
